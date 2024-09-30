ff4ea77ef51f545cc02dfb7c06110d9ff4bbed51
Augusto Pamplona <augustopamplonaa@gmail.com>
2024-09-30 17:10:01 -0300

    refactor: update enum code to deny depreciation

--- app/models/webhook.rb
+++ app/models/webhook.rb
@@ -5,5 +5,5 @@ class Webhook < ApplicationRecord
   validates :url, presence: true, format: { with: URI::DEFAULT_PARSER.make_regexp, message: "must be a valid URL" }
   validates :kind, presence: true
 
-  enum kind: { default: 0, client: 1, bill: 2, refund: 3 }
+  enum :kind, { default: 0, client: 1, bill: 2, refund: 3 }
 end
